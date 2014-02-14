fba8f7da264eb733b6ea651ffa6e9e1bb8fc6f9a
dani en curro <corneliusinlove@gmail.com>
2014-02-14 15:44:10 +0100

    haciendo la prueba de carga datatbles

--- cgi-bin/tests/data_tables_con_muchos_datos/cgi-bin/data_tables_con_muchos_datos.pl
+++ cgi-bin/tests/data_tables_con_muchos_datos/cgi-bin/data_tables_con_muchos_datos.pl
@@ -1,7 +1,35 @@
 #!/usr/bin/perl -w
 use strict;
+use Data::Dumper;
 print "Content-type: text/html\n\n";
- print <<'HTML';
+my $path_web = "/carga_datatables";
+
+# Cargar los datos de ipservicio_nodo.txt y devolverlo en un hash
+sub cargarHost($){
+	my $path_y_fichero_ip_servicio_nodo = shift;
+	my %ipservicio_nodo;
+	our $fallo_grave_mensaje;
+	open (IPSERV, $path_y_fichero_ip_servicio_nodo)  or die $fallo_grave_mensaje ." [u803ry893r] intentando abrir ".$path_y_fichero_ip_servicio_nodo." error[".$!."]";; # No hay control de errores por que se comprueba en bootApp
+	while(<IPSERV>){
+		next if $_!~/^\d/;   # pasar a siguiente si no comienza por un numero
+		my @datos_splited = split(/\s+/,$_);
+		$ipservicio_nodo{$datos_splited[0]} = $datos_splited[1];
+	}
+	close IPSERV;
+	return(%ipservicio_nodo);
+}
+
+
+# cargar host (uno ficticio)
+open(HOST, "hosts");
+my @host = (<HOST>);
+close(HOST);
+# print Dumper(@host);
+
+my %etc_hosts = cargarHost("hosts");
+print Dumper(%etc_hosts);
+
+ print <<HTML;
  <html>
  <head>
  <title>A Simple Perl CGI</title>
@@ -11,14 +39,14 @@ print "Content-type: text/html\n\n";
 		
 		<title>PRUEBA DE CARGA DATATABLES</title>
 		<style type="text/css" title="currentStyle">
-			@import "css/demo_page.css";
-			@import "css/demo_table.css";
+			\@import "$path_web/css/demo_page.css";
+			\@import "$path_web/css/demo_table.css";
 		</style>
-		<script type="text/javascript" language="javascript" src="js/jquery.js"></script>
-		<script type="text/javascript" language="javascript" src="js/jquery.dataTables.js"></script>
+		<script type="text/javascript" language="javascript" src="$path_web/js/jquery.js"></script>
+		<script type="text/javascript" language="javascript" src="$path_web/js/jquery.dataTables.js"></script>
 		<script type="text/javascript" charset="utf-8">
-			$(document).ready(function() {
-				$('#example').dataTable();
+			\$(document).ready(function() {
+				\$('#example').dataTable();
 			} );
 		</script>
 	</head>
@@ -42,6 +70,11 @@ print "Content-type: text/html\n\n";
 			<th>CSS grade</th>
 		</tr>
 	</thead>
+HTML
+foreach (@hosts){
+
+}
+print <<HTML;
 	<tbody>
 		<tr class="odd gradeX">
 			<td>Trident</td>
