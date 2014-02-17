773a53ddfaa8831b5ea22d159e52c830e075b911
dani en curro <corneliusinlove@gmail.com>
2014-02-17 11:58:02 +0100

    mejorado deploy (paths absolutos)

--- cgi-bin/tests/data_tables_con_muchos_datos/deploy_local_carga_datos.sh
+++ cgi-bin/tests/data_tables_con_muchos_datos/deploy_local_carga_datos.sh
@@ -1,2 +1,2 @@
-rsync -r cgi-bin/ /var/www/cgi-bin/carga_datatables/
-rsync -r html/ /var/www/html/carga_datatables/
+rsync -r  /home/dan/cosas_hechas/84.gestor_etc_hosts/cgi-bin/ /var/www/cgi-bin/carga_datatables/
+rsync -r /home/dan/cosas_hechas/84.gestor_etc_hosts/html/ /var/www/html/carga_datatables/
