a2784277dc0ce483c9e0d0ec7836567c2e92b972
dani en curro <corneliusinlove@gmail.com>
2014-02-17 18:27:37 +0100

    escribiendo las funciones para paginar

--- cgi-bin/tests/data_tables_con_muchos_datos/cgi-bin/data_tables_server_side.pl
+++ cgi-bin/tests/data_tables_con_muchos_datos/cgi-bin/data_tables_server_side.pl
@@ -4,9 +4,15 @@
 use strict;
 use Data::Dumper;
 use JSON;
+use CGI;
 print "Content-type: text/html\n\n";
+my @aaData; # aaData tiene q tener 2 dimensiones  [  [datos1, datos1],[datos2 , datos2] ]
+			# son los datos de la tabla
 
-
+my $q = CGI->new;
+my @param = $q->param;   # parametros llegados via url
+ 
+my $datos_log= "";
 # print Dumper(%json_mock);
 
 # Cargar los datos de ipservicio_nodo.txt y devolverlo en un hash
@@ -24,27 +30,47 @@ sub cargarHost($){
 	return(%ipservicio_nodo);
 }
 
-
-# cargar host (uno ficticio)
-# open(HOST, "hosts");
-# my @hosts = (<HOST>);
-# close(HOST);
-# print Dumper(@hosts);
+# cargar los etch_hosts
 
 my %etc_hosts = cargarHost("hosts");
+my $num_total_records = keys(%etc_hosts);
 my %json_mock;
 $json_mock{sEcho} = 1;
-$json_mock{iTotalRecords} = 57;
-$json_mock{iTotalDisplayRecords} = 57;
-my @aaData = (["IP1", "HOSTNAME1"],["IP2", "HOSTNAME2"]);
+$json_mock{iTotalRecords} = $num_total_records;
+
+my $contador_display_length = $q->param('iDisplayLength');
+my $contador_display_start = $q->param('iDisplayStart');
+my $contador = 0;
+foreach my $ip(keys(%etc_hosts)){
+	next if $contador < $contador_display_start;
+	# if($ip =~ /10.250/){
+	push(@aaData,[$ip, $etc_hosts{$ip}]);   # no vale que aaData sea un array con los datos, tiene que ser un array de arrays
+
+	# }
+	$contador++;
+	last if $contador == $contador_display_length;
+}
+
+$json_mock{iTotalDisplayRecords} = $num_total_records;
+
 $json_mock{aaData} = \@aaData;
-
  my $json=new JSON;
-
-
-			# foreach (keys(%etc_hosts)){
-			# }
-
 
 my $encoded = $json->encode(\%json_mock);
 print $encoded;
+
+
+
+##############################
+# para ver los param por LOG #
+##############################
+open (LOG,">>/tmp/log_carga_datos") || die "problema abriedo /tmp/log_carga_datos";
+foreach my $unparam(@param){
+	my $value = $q->param($unparam);
+	$datos_log .= $unparam."\t".$value."\n";
+	# $q->save(\*LOG);  # no he coseguido que funcione  http://perldoc.perl.org/CGI.html#SAVING-THE-STATE-OF-THE-SCRIPT-TO-A-FILE%3a
+}
+$datos_log .= "estado PARAMETROS contador=[".$contador."] contador_display_start=[".$contador_display_start."] contador_display_length=[".$contador_display_length."]\n";
+print LOG $datos_log."=================\n";
+close(LOG);
+
